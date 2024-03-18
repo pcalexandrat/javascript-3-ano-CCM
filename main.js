499c87fe31861433215f6e3d156fb87baf55bcee
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-03-18 10:23:13 -0300

    aba-conteudo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
 for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
