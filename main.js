3b60cedf44ed49560f006cde09cbbe825df03ade
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-03-19 09:15:08 -0300

    textos

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
-            botoes[j].classList.remove("textos");
+            textoss[j].classList.remove("textos");
         }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
-
+const botoes = document.querySelectorAll(".conteudo");
+const tempoObjetivo1 = new Date("2024);
