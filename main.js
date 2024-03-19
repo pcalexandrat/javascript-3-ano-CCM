f3df4d6b655e1469f22b1d5da05e292751abf879
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-03-19 19:43:33 -0300

    ativo

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ for(let i=0; i<botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("textos");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
