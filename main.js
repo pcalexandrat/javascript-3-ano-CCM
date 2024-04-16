ab25c0f694877ededc50d9e3b2a82d877d7e1c57
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-04-16 19:57:12 -0300

    for

--- main.js
+++ main.js
@@ -16,7 +16,12 @@ const tempoObjetivo2 = new Date("2024-09-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-05T00:00:00");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+for (let i=0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);  
+}
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
