f3af342ae1bcda1f47c23618fc8770ae4c1004e3
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-04-16 20:00:19 -0300

    2025

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-09-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
