29db2bca60edda6302fa43fab7a08ddb5777d24a
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-04-23 20:11:55 -0300

    PRAZO FINALIZADO

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-    return "Prazo Finalizado";
+    return "PRAZO FINALIZADO ;(";
     }
 
 }
