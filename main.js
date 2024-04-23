a00692754bb729f8e3d3246b40e83016c51ff943
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-04-23 20:04:58 -0300

    prazo finalizado

--- main.js
+++ main.js
@@ -30,7 +30,16 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24; 
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+    return "Prazo Finalizado";
+    }
+
 }
 
 function atualizaCronometro(){
