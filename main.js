59be998094539ef960f803a9ce3ba91dc1c6d2e0
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2026-05-13 11:15:29 -0300

    Update countdown target dates and fix logic

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ for(let i=0; i<botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-03-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-09-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-06-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-09-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo4 = new Date("202-02-05T00:00:00");
 
